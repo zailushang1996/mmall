d79c500b05f565792099b1e6b05a2a9f08a0acd3
zailushang1996 <2212926237@qq.com>
2018-08-26 15:36:44 +0800

    用户模块

--- src/main/java/com/mmall/common/ServerResponse.java
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -1,12 +1,17 @@
 package com.mmall.common;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
 import java.io.Serializable;
 
 /**
  * @author LiuZX liuzhixiang
  * DATE   2018/8/25
+ *
  */
-
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+//保证序列化json的时候,如果是null的对象,key也会消失
 public class ServerResponse<T> implements Serializable {
     /**
      * 状态码
@@ -23,6 +28,7 @@ public class ServerResponse<T> implements Serializable {
 
     /**
      * 私有构造器
+     *
      * @param status
      */
     private ServerResponse(int status) {
@@ -31,6 +37,7 @@ public class ServerResponse<T> implements Serializable {
 
     /**
      * 私有构造器
+     *
      * @param status
      * @param data
      */
@@ -41,6 +48,7 @@ public class ServerResponse<T> implements Serializable {
 
     /**
      * 私有构造器
+     *
      * @param status
      * @param msg
      * @param data
@@ -53,6 +61,7 @@ public class ServerResponse<T> implements Serializable {
 
     /**
      * 私有构造器
+     *
      * @param status
      * @param msg
      */
@@ -61,6 +70,8 @@ public class ServerResponse<T> implements Serializable {
         this.msg = msg;
     }
 
+    @JsonIgnore
+    //使之不在json序列化结果当中
     public boolean isSuccess() {
         return this.status == ResponseCode.SUCCESS.getCode();
     }
@@ -84,4 +95,27 @@ public class ServerResponse<T> implements Serializable {
     public static <T> ServerResponse<T> createBySuccessMessage(String msg) {
         return new ServerResponse<>(ResponseCode.SUCCESS.getCode(), msg);
     }
+
+    public static <T> ServerResponse<T> createBySuccess(T data) {
+        return new ServerResponse<>(ResponseCode.SUCCESS.getCode(), data);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(String msg, T data) {
+        return new ServerResponse<>(ResponseCode.SUCCESS.getCode(), msg, data);
+    }
+
+    public static <T> ServerResponse<T> createByError() {
+        return new ServerResponse<>(ResponseCode.ERROR.getCode(), ResponseCode.ERROR.getDesc());
+    }
+
+
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMessage) {
+        return new ServerResponse<>(ResponseCode.ERROR.getCode(), errorMessage);
+    }
+
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode, String errorMessage) {
+        return new ServerResponse<>(errorCode, errorMessage);
+    }
+
+
 }
--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -1,5 +1,8 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
 /**
  * Author:liuzhixiang
  * Create by LiuZX on 2018/8/25
@@ -8,5 +11,17 @@ package com.mmall.service;
  */
 public interface IUserService {
 
-    Object login(String username, String password);
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse<String> selectQuestion(String username);
+
+    ServerResponse<String> forgetCheckAnswer(String username, String question, String answer);
+
+    ServerResponse<String> forgetResetPassword(String username, String newPassword, String forgetToken);
+
+    ServerResponse<String> resetPassword(User user, String oldPassword, String newPassword);
 }

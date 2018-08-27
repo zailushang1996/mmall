25b73dd54717e2476feac361524e93b9e284bb78
zailushang1996 <2212926237@qq.com>
2018-08-27 17:37:13 +0800

    windows下FTPserver服务器集成 nginx与host配合实现FTP服务器代理 文件上传

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -18,34 +18,42 @@ import java.util.UUID;
  */
 @Service("iFileService")
 public class FileServiceImpl implements IFileService {
-    private static final Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
-    public String upload(MultipartFile file, String path) {
+
+    private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+
+
+    public String upload(MultipartFile file,String path){
         String fileName = file.getOriginalFilename();
         //扩展名
-        String fileExtensionName = fileName.substring(fileName.lastIndexOf(".") + 1);
-        String uploadFileName = UUID.randomUUID().toString() + "." + fileExtensionName;
-        logger.info("开始上传文件，上传文件的文件名：{}，上传的路径是：{},新文件名：{}", fileName, path, uploadFileName);
+        //abc.jpg
+        String fileExtensionName = fileName.substring(fileName.lastIndexOf(".")+1);
+        String uploadFileName = UUID.randomUUID().toString()+"."+fileExtensionName;
+        logger.info("开始上传文件,上传文件的文件名:{},上传的路径:{},新文件名:{}",fileName,path,uploadFileName);
+
         File fileDir = new File(path);
-        if (!fileDir.exists()) {
+        if(!fileDir.exists()){
             fileDir.setWritable(true);
             fileDir.mkdirs();
         }
-        File targetFile = new File(path, uploadFileName);
+        File targetFile = new File(path,uploadFileName);
+
 
         try {
             file.transferTo(targetFile);
-            //文件已经上传成功
+            //文件已经上传成功了
 
-            //todo 将targetFile上传到FTP服务器上
+
             FTPUtil.uploadFile(Lists.newArrayList(targetFile));
-            //已经上传到FTP服务器
-            targetFile.delete();
+            //已经上传到ftp服务器上
 
-            //todo 上传到FTP服务器后，删除upload文件夹下的文件
+            targetFile.delete();
         } catch (IOException e) {
-            logger.error("上传文件异常", e);
+            logger.error("上传文件异常",e);
             return null;
         }
+        //A:abc.jpg
+        //B:abc.jpg
         return targetFile.getName();
     }
+
 }

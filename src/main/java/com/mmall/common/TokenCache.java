ce464add0baade7d0438039d6c9f3c56c59fc989
zailushang1996 <2212926237@qq.com>
2018-09-25 17:40:00 +0800

    ""

--- src/main/java/com/mmall/common/TokenCache.java
+++ src/main/java/com/mmall/common/TokenCache.java
@@ -17,15 +17,16 @@ public class TokenCache {
 
     public static final String TOKEN_PREFIX = "token_";
 
-    private static LoadingCache<String, String> localCache = CacheBuilder.newBuilder().initialCapacity(1000).maximumSize(10000).expireAfterAccess(12,TimeUnit.HOURS)
-            .build(new CacheLoader<String, String>() {
-        //默认的数据加载实现，当调用get取值的时候，如果key没有对应的值，就调用这个方法进行加载
-        @Override
-        public String load(String s) throws Exception {
+    private static LoadingCache<String, String> localCache = CacheBuilder.newBuilder()
+            .initialCapacity(1000)
+            .maximumSize(10000)
+            .expireAfterAccess(12,TimeUnit.HOURS).build(new CacheLoader<String, String>() {
+                @Override
+                public String load(String key) throws Exception {
+                    return "null";
+                }
+            });
 
-            return "null";
-        }
-    });
 
     public static void setKey(String key, String value) {
         localCache.put(key,value);
